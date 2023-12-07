986bf5fdf23f05715120097426f30c141e78db37
Matej DB <mdrazi01@fesb.hr>
2023-12-07 17:50:28 +0100

    minor setup of Voting contract

--- test/Voting.t.sol
+++ test/Voting.t.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {Voting} from "../src/Voting.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
+    Voting public counter;
 
     function setUp() public {
-        counter = new Counter();
+        counter = new Voting();
         counter.setNumber(0);
     }
 
